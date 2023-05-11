0be5a403490f4117f77f2602d8991f2b022999d9
yuanlu9707 <80336181+yuanlu9707@users.noreply.github.com>
2023-05-11 16:35:14 +0800

    Delete PSSP_8state directory

--- PSSP_8state/SEBTCN/initializeGaussian.m
+++
@@ -1,6 +0,0 @@
-%Weights Initialization Function
-function parameter = initializeGaussian(sz)
-
-parameter = randn(sz,'single') .* 0.01;
-
-end
--- PSSP_8state/SEBTCN/thresholdL2Norm.m
+++
@@ -1,9 +0,0 @@
-%Gradient Clipping Function
-function g = thresholdL2Norm(g,gradientThreshold)
-
-gradientNorm = sqrt(sum(g.^2,'all'));
-if gradientNorm > gradientThreshold
-    g = g * (gradientThreshold / gradientNorm);
-end
-
-end
